17085c8040654cdf53b68d3b531b43ddc6025239
Dmitriy <plotnikovdi95@gmail.com>
2023-03-31 14:29:00 +0300

    added selection menu items and changing link

--- app/main_bp/views.py
+++ app/main_bp/views.py
@@ -8,3 +8,15 @@ def index():
 @main.get("/reject-form")
 def reject_form():
     return render_template("reject/reject_form.html")
+
+@main.get("/services")
+def reject_services():
+    return render_template("reject/services.html")
+
+@main.get("/blog")
+def reject_blog():
+    return render_template("reject/blog.html")
+
+@main.get("/products")
+def reject_products():
+    return render_template("reject/products.html")
